66b19bee7e811e613cc5d26c0698573c3903377c
ruckus82 <dieruckusdie@gmail.com>
2016-12-22 16:56:50 +0200

    room leave fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule}   from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot([
+            {
+                path: 'login',
+                component: LoginComponent
+            },
+            {
+                path: 'register',
+                component: RegisterComponent
+            }
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/components/register/register.component.js
@@ -0,0 +1,42 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var core_1 = require('@angular/core');
+var RegisterComponent = (function () {
+    function RegisterComponent() {
+    }
+    RegisterComponent.prototype.alertMessage = function () {
+    };
+    RegisterComponent.prototype.guideMessage = function (stage) {
+        switch (stage) {
+            case 1:
+                this.message = 'Да, с имени и нужно начинать';
+                break;
+            case 2:
+                this.message = 'Молодец, теперь пароль';
+                break;
+            case 3:
+                if (this.password === this.password2) {
+                    this.message = 'Красава, пароли совпали, жми "Регистрироваться"!';
+                }
+                else {
+                    this.message = 'Давай, ещё разок пароль вводи...';
+                }
+                break;
+        }
+    };
+    RegisterComponent = __decorate([
+        core_1.Component({
+            selector: 'register',
+            templateUrl: './register.component.html',
+            styleUrls: ['./register.component.less']
+        })
+    ], RegisterComponent);
+    return RegisterComponent;
+}());
+exports.RegisterComponent = RegisterComponent;
+//# sourceMappingURL=register.component.js.map
